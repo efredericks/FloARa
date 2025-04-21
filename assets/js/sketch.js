164ec4f6ebd90c332933eb42270834efa58d0e22
Erik Fredericks <frederer@gvsu.edu>
2025-04-21 09:17:54 -0400

    basic responsiveness

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -2,11 +2,17 @@
 // https://github.com/processing/p5.js/wiki/Optimizing-p5.js-Code-for-Performance#p5-performance-tips
 p5.disableFriendlyErrors = true; // disables FES
 
+// global vars
+let bg;
+
 // load in background and flowers at full resolution
 function preload() {
+  bg = loadImage("../assets/img/gvsu-bg.jpg");
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER);
+
   drawEverything();
 }
 function draw() {
@@ -15,6 +21,24 @@ function draw() {
 // draw everything with respect to the canvas size
 function drawEverything() {
   background(0);
+
+  // force landscape mode
+  if (width < height) {
+    push();
+    fill(255);
+    textSize(width * 0.05);
+    textAlign(CENTER, CENTER);
+    translate(width * 0.5, height * 0.5);
+    text("Please rotate your device", 0, 0);
+    pop();
+  } else {
+    background(0);
+
+    // landscape image - maintain aspect ratio wrt width
+    let w_aspect = bg.width / width;
+    let h = bg.height / w_aspect;
+    image(bg, 0, 0, width, h, 0, 0, bg.width, bg.height);
+  }
 }
 
 // resize all images wrt aspect ratio
