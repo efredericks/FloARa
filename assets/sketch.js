12cac4d534f2a1c2976927993beec12d323d9a50
Erik Fredericks <frederer@gvsu.edu>
2025-04-21 07:10:12 -0400

    tweaking css

--- assets/sketch.js
+++ assets/sketch.js
@@ -2,18 +2,28 @@
 // https://github.com/processing/p5.js/wiki/Optimizing-p5.js-Code-for-Performance#p5-performance-tips
 p5.disableFriendlyErrors = true; // disables FES
 
+// load in background and flowers at full resolution
 function preload() {
 }
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  drawEverything();
+}
+function draw() {
+}
+
+// draw everything with respect to the canvas size
+function drawEverything() {
   background(0);
 }
 
-function draw() {
+// resize all images wrt aspect ratio
+function resizeImages() {
+
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  background(0);
+  resizeImages();
+  drawEverything();
 }
