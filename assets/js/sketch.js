8a9c77f2e735e8e464a5c5ffcb306a4d00d0eb26
Erik Fredericks <frederer@gvsu.edu>
2025-05-13 22:12:59 -0400

    setup local plant mapping and growth difference

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -13,14 +13,19 @@ let wind_material;
 let dither_fs, tv_fs, rgb_fs;
 let shaders_on, touch_timer;
 
-let temp_milkweed;
-let temp_nymphaea;
+let QR_map = {
+  0: { name: 'Milkweed', scale: 0.4 },
+  1: { name: 'Nymphaea', scale: 0.04 },
+  99: { name: 'Piranha', scale: 0.4 },
+}
+let plant_images = {};
 
 // temp variables
 let debug = false;
 let wind_on = true;
 
 let font;
+let ctx;
 
 // load in background and flowers at full resolution
 function preload() {
@@ -33,8 +38,15 @@ function preload() {
   overlay = loadImage("assets/img/131028_Fall_Campus-6934_Pano-2.overlay.png");
 
   // temp_milkweed = loadImage("assets/img/milkweed/Milkweed_0000_5_sm.cropped.png");
-  temp_milkweed = loadImage("assets/img/milkweed/Milkweed_5_outerglow.png");
-  temp_nymphaea = loadImage("assets/img/Nymphaea-Odorata-Ella-Kane/nymphaea_odorata_stage5.png");
+
+  plant_images[QR_map[0].name] = [];
+  plant_images[QR_map[1].name] = [];
+  plant_images[QR_map[99].name] = [];
+
+  for (let i = 1; i <= 5; i++) {
+    plant_images[QR_map[0].name].push(loadImage("assets/img/milkweed/Milkweed_5_outerglow.png"));
+    plant_images[QR_map[1].name].push(loadImage(`assets/img/Nymphaea-Odorata-Ella-Kane/nymphaea_odorata_stage${i}.png`));
+  }
 
   font = loadFont("assets/fonts/Quicksand-Medium.ttf");
 }
@@ -42,6 +54,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   pixelDensity(1);
+  noSmooth();
 
   textFont(font);
   textAlign(CENTER);
@@ -73,7 +86,6 @@ function setup() {
   touch_timer = 0;
 
   flowers = setupRandomData(bg.width, bg.height, mask, 250);
-  // wind_fs = temp_milkweed_gfx.createFilterShader(wind_src);
 
   drawEverything();
 
@@ -106,7 +118,6 @@ function draw() {
     filter(dither_fs);
   }
 
-  console.log(touches.length);
   if (touches.length > 2) {
     if (touch_timer == 0)
       shaders_on = !shaders_on;
@@ -131,7 +142,7 @@ function drawEverything() {
     fill(255);
     textSize(width * 0.05);
     textAlign(CENTER, CENTER);
-    translate(width * 0.5, height * 0.5);
+    // translate(width * 0.5, height * 0.5);
     text("Please rotate your device", 0, 0);
     pop();
   } else {
@@ -150,6 +161,7 @@ function drawEverything() {
     }
 
     let i = 0;
+    let now = new Date();
     for (let f of flowers) {
       let h_aspect = bg.height / h;
       let x = (f.location.x / w_aspect);
@@ -158,15 +170,17 @@ function drawEverything() {
       // perspective for 'farther away'
       let sc = map(y, height, height * 0.2, 1.0, 0.001);
       let _w, _h, _img;
-      if (f.QR_id == 0) {
-        _w = (temp_milkweed.width * 0.4) * sc;
-        _h = (temp_milkweed.height * 0.4) * sc;
-        _img = temp_milkweed;
-      } else {
-        _w = (temp_nymphaea.width * 0.04) * sc;
-        _h = (temp_nymphaea.height * 0.04) * sc;
-        _img = temp_nymphaea;
-      }
+
+      // currently a day will change the plant
+      let date_diff = Math.floor(dateDifference(now, f.timestamp));
+      let idx = 0;
+      if ((date_diff / 5) > 4) idx = 4;
+      else idx = Math.floor(date_diff / 5);
+
+      _img = plant_images[QR_map[f.QR_id].name][idx];
+      _w = (_img.width * QR_map[f.QR_id].scale) * sc;
+      _h = (_img.height * QR_map[f.QR_id].scale) * sc;
+
 
       // magic numbers help with offset within image
       push();
@@ -196,23 +210,8 @@ function drawEverything() {
 function loadData() {
 }
 
-// resize all images wrt aspect ratio
-function resizeImages() {
-
-}
-
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  // temp_milkweed_gfx = createGraphics(width, height);
-
-  // for (let f of flowers) {
-  //   let w_aspect = bg.width / width;
-  //   let h = bg.height / w_aspect;
-  //   let h_aspect = bg.height / h;
-  //   temp_milkweed_gfx.image(temp_milkweed, f.location.x / w_aspect, f.location.y / h_aspect);
-  // }
-
-  resizeImages();
   drawEverything();
 }
 
@@ -241,4 +240,9 @@ function getPixelID(x, y, g = null) {
     idx = 4 * d * (int(y) * d * g.width + int(x));
   }
   return idx;
+}
+
+// return difference in days
+function dateDifference(start, end) {
+  return (start - end) / (1000 * 3600 * 24);
 }
