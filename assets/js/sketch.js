04239b448127ec21ab614fb4a683d0eeb3d8fa1a
Erik Fredericks <frederer@gvsu.edu>
2025-07-26 15:58:31 -0400

    using half size for bg and not resizing

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -166,9 +166,11 @@ let plant_images_orig;
 function preload() {
   // keeping a hires version, but scaling to 50% immediately as mobile chrome can't handle it
   hi_res_bg = loadImage("assets/img/BG-Retouch.jpg");
-  bg = loadImage("assets/img/BG-Retouch.jpg");
+  bg = loadImage("assets/img/BG-Retouch-Half.jpg");
+  // bg = loadImage("assets/img/BG-Retouch.jpg");
   hi_res_mask = loadImage("assets/img/BG-Retouch-mask.png");
-  mask = loadImage("assets/img/BG-Retouch-mask.png");
+  mask = loadImage("assets/img/BG-Retouch-mask-half.png");
+  // mask = loadImage("assets/img/BG-Retouch-mask.png");
 
   overlay = loadImage("assets/img/131028_Fall_Campus-6934_Pano-2.overlay.png");
 
@@ -262,8 +264,8 @@ function setup() {
   // pixelDensity(1);
   noSmooth();
 
-  bg.resize(bg.width * 0.5, 0);
-  mask.resize(mask.width * 0.5, 0);
+  // bg.resize(bg.width * 0.5, 0);
+  // mask.resize(mask.width * 0.5, 0);
 
   textFont(font);
   textAlign(CENTER);
